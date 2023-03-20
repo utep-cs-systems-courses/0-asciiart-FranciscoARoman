1ea9a823011584cca35749e79eb149a1ca53df8a
FranciscoARoman <faroman1@miners.utep.edu>
2023-03-20 12:05:15 -0600

    add changed files for project 0

--- draw_shapes.c
+++ draw_shapes.c
@@ -26,3 +26,22 @@ void print_triangle(int leftCol, int size)
   }
 }
 
+// Prints an Arrow of specified height
+void print_arrow(int leftCol, int size) // (7,5)
+{
+  for (int row = 0; row <= size; row++){
+    int minCol = leftCol + size - row, maxCol = leftCol + size + row;
+    int col;
+    for (col = 0; col < minCol; col++) putchar(' ');
+    for (       ; col <= maxCol; col++) putchar('*');
+    putchar('\n');
+  }
+  int i, j;
+  int endCol = leftCol + size;
+  for (int row = 0; row < size; row++){
+    int col;
+    for (col = 0; col < leftCol+leftCol/2; col++) putchar(' ');
+    for (       ; col < endCol+leftCol/2; col++) putchar('*');
+    putchar('\n');
+  }
+}
